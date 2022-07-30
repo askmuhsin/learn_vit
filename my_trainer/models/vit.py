d18caae9d817e58dd69527bb063ccaecbcac2917
Muhsin <askmuhsin@gmail.com>
2022-07-30 21:15:05 +0530

    added multi head attention module

--- my_trainer/models/vit.py
+++ my_trainer/models/vit.py
@@ -17,7 +17,7 @@ class PatchEmbeddings(nn.Module):
         in_channels: int, number of input channels (default 3 for RGB)
         embedding_size: int, the projection size (default 768)
     
-    Attributes:
+    Learnables:
         projection: nn.Conv2d, a learnable layer of dim - 768 X [16 X 16 X 3]
         
     """
@@ -46,4 +46,72 @@ class PatchEmbeddings(nn.Module):
         x = self.projection(x) ## (n_samples, embedding_size, n_p_h, n_p_w)
         x = x.flatten(2)       ## (n_samples, embedding_size, n_patches)
         x = x.transpose(1, 2)  ## (n_samples, n_patches, embedding_size)
+        return x
+
+
+class Attention(nn.Module):
+    """
+    applies the multi Attention mechanism to the input.
+    the weights for q, k, v, for all the multi head attentions are initialized together.
+
+    Parameters:
+        dim: int, the input dim from patchEmbeddings (default 768)
+        n_heads: int, number of multi head attention layers
+        attn_p: float, probability for Dropout on attention weight
+        proj_p: float, probability for Dropout on projection out
+    
+    Learnables:
+        qkv: nn.Linear, q, k, v weights for all heads are initilized here
+        proj: nn.Linear, a projection of same size as input after applying attention weights
+
+    """
+    def __init__(
+        self,
+        dim=768,
+        n_heads=12,
+        attn_p=.0,
+        proj_p=.0,
+    ):
+        super().__init__()
+        
+        self.dim = dim
+        self.n_heads = n_heads
+        self.head_dims = dim // n_heads  ## defautls to 64 (dim - 768, n_heads - 12)
+        self.scale = self.head_dims ** -.5
+        
+        self.qkv = nn.Linear(dim, dim * 3, bias=True)
+        self.attn_dropout = nn.Dropout(attn_p)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_dropout = nn.Dropout(proj_p)
+        
+    def forward(self, x):
+        """
+        Parameters:
+            x: torch.Tensor, shape `(n_samples, n_tokens, embedding_size)`
+
+        Returns:
+            torch.Tensor, shape `(n_samples, n_tokens, embedding_size)`
+        """
+        n_samples, n_tokens, dim = x.shape
+        
+        qkv = self.qkv(x)  ## (n_samples, n_tokens, embedding_size * 3)
+        qkv = qkv.reshape(
+            n_samples, n_tokens, 3, self.n_heads, self.head_dims
+        ) ## (n_samples, n_tokens, 3, n_heads, head_dims)
+        qkv = qkv.permute(2, 0, 3, 1, 4) ## (3, n_samples, n_heads, n_tokens, head_dims)
+        
+        q, k, v = qkv ## (n_samples, n_heads, n_tokens, head_dims)
+        k_t = k.transpose(-1, -2) ## (n_samples, n_heads, head_dims, n_tokens)
+        
+        attn_weights = (q @ k_t) * self.scale  ## (n_samples, n_heads, n_tokens, n_tokens)
+        attn_weights = attn_weights.softmax(dim=-1)
+        attn_weights = self.attn_dropout(attn_weights)
+        
+        weighted_val = attn_weights @ v  ## (n_samples, n_heads, n_tokens, head_dims)
+        weighted_val = weighted_val.transpose(1, 2)  ## (n_samples, n_tokens, n_heads, head_dims)
+        weighted_val = weighted_val.flatten(2)  ## (n_samples, n_tokens, dim)
+        
+        x = self.proj(weighted_val)  ## (n_samples, n_tokens, dim)
+        x = self.proj_dropout(x)
+        
         return x
